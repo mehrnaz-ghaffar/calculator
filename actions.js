51fd483ebc79998f7035e088726d8bdc61e63e82
mehhrnaz-ghaffar <mehrnaz.ghaffar@gmail.com>
2021-09-17 19:29:47 +0430

    final touches

--- actions.js
+++ actions.js
@@ -1,142 +1,86 @@
-class calculator {
-    constructor(previousElement  , currentElement){
-        this.previousElement = previousElement
-        this.currentElement = currentElement
-        clear()
+function getHistory(){
+    return document.getElementById("history-value").innerText
+}
+
+function printHistory(number){
+    document.getElementById("history-value").innerText = number
+}
+
+function getResult(){
+    return document.getElementById("result-value").innerText
+}
+
+function printResult(number){
+    if(number == ""){
+        document.getElementById("result-value").innerText = number
+    }
+    else{
+        document.getElementById("result-value").innerText = formattedNumber(number)
     }
 }
 
-
-const numberButtons = document.getElementById("number").innerText
-const operationButtons = document.getElementById("operation").innerText
-const equalButton = document.getElementById("equal").innerText
-const DeleteButton = document.getElementById("delete").innerText
-const clearButton = document.getElementById("clear").innerText
-const previousElement = document.getElementById("previous").innerText
-const currentElement = document.getElementById("current").innerText
- 
-const Calculator = new calculator(previousElement , currentElement)
-
-
-//////////////// methods ///////////////
-function clear() {
-    this.currentOperand = ''
-    this.previousOperand = ''
-    this.operation = undefined
+function formattedNumber(num){
+    if(num == "-"){
+        return ""
+    }
+    var n = Number(num)
+    var value = n.toLocaleString("en")
+    return value
 }
 
-function Delete(){
-    this.currentOperand = this.currentOperand.toSring().slice(0.-1)
+function reverseFormat(num){
+    return Number(num.replace(/,/g , ''))
 }
 
-function compute(){
-
-    let result
-    const prev = parseFloat(this.previousOperand)
-    const cur = parseFloat(this.currentOperand)
-    if( isNaN(prev) || isNaN(cur)) return
-
-    switch(this.operation){
-        case '+' :
-            return result = prev + cur
-            break
-        case '-' :
-            return result = prev - cur
-            break
-        case 'x' :
-            return result = prev * cur
-            break
-        case '/' :
-            return result = prev / cur
-            break
-        default:
-            return
-            {
-                this.currentOperand = result
-                this.operation = undefined
-                this.previousOperand = ''
+var operator = document.getElementsByClassName("operator")
+for(var i = 0 ; i< operator.length ; i++){
+    operator[i].addEventListener('click', function(){
+        if(this.id == "clear"){
+            printResult("")
+            printHistory("")
+        }
+        else if(this.id == "delete"){
+            var output = reverseFormat(getResult()).toString()
+            //if output has value
+            if(output){
+                output = output.substr(0 , output.length-1)
+                printResult(output)
             }
         }
-    }
-
-function selectOperation(operation){
-    if( this.currentOperand === '') return
-    if(this.previousOperand !== ''){
-        compute()
-    }
-    this.operation = operation
-    this.previousOperand = this.currentOperand
-    this.currentOperand = ''
-}
-
-function appendNumber(number){
-    if (number ==='.' && currentOperand.includes('.')) return
-    this.currentOperand = this.currentOperand.toLocaleString() + number.toLocaleString()
-       
-}
-
-function getDisplay (number){
-    const numberString = number.toLocaleString()
-    const integerDigits = parseFloat(numberString.split('.')[0])
-    const decimalDigits = numberString.split('.')[1]
-    let integerDisplay
-    if(isNaN(integerDigits)) {
-        integerDisplay =  ''
-    }
-    else{
-        integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
-    }
-    if (decimalDigits != null) {
-        return `${integerDisplay}.${decimalDigits}` ////////
-    } 
-    else{
-        return integerDisplay
-    }
-}
-
-function display() {
-    this.currentElement.textContent = this.getDisplay(this.currentOperand)
-    if (this.operation != null){
-        this.previousElement.textContent =
-          `${this.getDisplay(this.previousOperand)} ${this.operation}`
-    }
-    else{
-        this.previousElement.innerText = ''
-    }
-}
-
-//////////// Listeners ///////////////
-clearButton.addEventListener('click' , button => {
-    clear()
-    display()
-})
-
-DeleteButton.addEventListener('click' , button => {
-    Delete()
-    display()
-})
-
-equalButton.addEventListener('click' , button => {
-    compute()
-    display()
-})
-
-operationButtons.forEach(button => {
-    button.addEventListener('click' , () => {
-        selectOperation(button.innerText)
-        display()
+        else{
+            var output = getResult()
+            var history = getHistory()
+            if(output=="" && history !=""){
+                if(isNaN(history[history.length-1])){
+                    history = history.substr(0 , history.length-1)
+                }
+            }
+            if(output != "" || history != ""){
+                output = output==""? output:reverseFormat(output)
+                history =history + output
+                if(this.id =="equal"){
+                    var result = eval(history)
+                    printResult(result)
+                    printHistory("")
+                }
+                else{
+                    history = history + this.id
+                    printHistory(history)
+                    printResult("")
+                }
+            }
+        }
     })
-})
+}
 
-numberButtons.forEach(button => {
-    button.addEventListener('click' , () => {
-        appendNumber(button.innerText)
-        display()
+var number = document.getElementsByClassName("number")
+for(var i = 0 ; i< number.length ; i++){
+    number[i].addEventListener('click', function(){
+        var output = reverseFormat(getResult())
+        //if output is number
+        if(output != NaN){
+            output = output + this.id
+            printResult(output)
+        }
     })
-})
-
-/*
-const arr = ["sal" , "hi"]
-console.log(arr.toLocaleString())
-help = document.getElementById("previous").innerText
-alert(help)*/
+}
